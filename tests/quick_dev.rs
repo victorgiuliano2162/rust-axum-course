3dba2a0676502e41b4d9a936802f956385c70e5b
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-06 20:05:02 -0300

    typo

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,7 +7,7 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello?name-Jen").await?.print().await?;
+    hc.do_get("/hello?name=Jen").await?.print().await?;
 
     Ok(())
 }
