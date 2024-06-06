6a5aa30fa49f06ccf57b22412c9463123f7d0c8a
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-06 19:55:08 -0300

    route and socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{extract::Query, response::{Html, IntoResponse}, routing::get, Router, Server};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(handler_hello),
+    );
+
+    //Start serve
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("-> Listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+
+    //Finish server
 }
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("-> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("Carlito");
+    Html("Hello <strong>{name}</strong>")
+}
+
+
