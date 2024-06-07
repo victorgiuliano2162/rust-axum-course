fcd19fe02e6090cc3d5d073d98b8c88f8800ef6c
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-06 21:55:36 -0300

    api test

---
+++ src/exemple/tests/quick_dev.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+
+use anyhow::Result;
+//Lib for tests
+
+#[tokio::main]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/hello2/Cargo").await?.print().await?;
+    //hc.do_get("/src/main.rs").await?.print().await?;
+
+    Ok(())
+}
