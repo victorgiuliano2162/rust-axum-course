6392bf07d0158ee4cc95a985385bb1758f46b3e1
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-08 20:13:00 -0300

    news routers and modifications in api_login method

--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,11 +1,37 @@
-use axum::Json;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use crate::{Error, Result};
+use crate::{web, Error, Result};
+use axum::routing::post;
+use axum::{Json, Router};
+use serde::Deserialize;
+use serde_json::{Value, json};
+use tower_cookies::{Cookie, Cookies};
 
-async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+
+
+pub fn routes() -> Router {
+    Router::new().route("/api/login", post(api_login))
+}
+
+
+async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>> {
     println!("-> {:<12} - api_login", "HANDLER");
-    todo!()
+    //TODO: implement real db/auth logic
+
+    if payload.username != "demo1" ||  payload.pwd != "welcome" {
+        return Err(Error::LoginFail);
+    } //it's just for the test
+    
+    //FIXME: implement real auth-token generation/signature
+    cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
+    //TODO: Set cookies
+
+    //Create the sucess body
+    let body = Json(json!({
+    "result": {
+        "sucess": true
+    }
+    }));
+
+    Ok(body)
 }
 
 #[derive(Debug, Deserialize)]
