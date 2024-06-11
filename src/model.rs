9ec50973543ba127370548f0df5f794546c06125
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-10 23:03:23 -0300

    controller implements create, get and delete

--- src/model.rs
+++ src/model.rs
@@ -50,6 +50,22 @@ impl ModelController {
 
         Ok(ticket)
     }
+
+    pub async fn list_tickets(&self) -> Result<Vec<Ticket>> {
+        let store = self.tickets_store.lock().unwrap();
+
+        let tickets = store.iter().filter_map(|t| t.clone()).collect();
+
+        Ok(tickets)
+    }
+
+    pub async fn delete_ticket(&self, id: u64) -> Result<Ticket> {
+        let mut store = self.tickets_store.lock().unwrap();
+
+        let ticket = store.get_mut(id as usize).and_then(|t| t.take());
+
+        ticket.ok_or(Error::TicketDeleteFailIdNotFound { id })
+    }
 }
 
 // end region -- Model Controller
