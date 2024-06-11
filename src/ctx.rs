3c19987aec69c8babbc90742e57b347d761dfa52
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-11 19:08:13 -0300

    added context mod to create, list and delete routes methods

---
+++ src/ctx.rs
@@ -0,0 +1,21 @@
+
+#[derive(Clone, Debug)]
+pub struct Ctx {
+    user_id: u64,
+}
+
+//Constructor
+impl Ctx {
+    pub fn new (user_id: u64) -> Self {
+        Self {
+            user_id
+        }
+    }
+}
+
+//Property acess
+impl Ctx {
+    pub fn user_id(&self) -> u64 {
+        self.user_id
+    }
+}
