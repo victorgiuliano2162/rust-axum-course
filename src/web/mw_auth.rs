8a4bf0fd3b954a552cc03045336154cb8b7f92e2
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-11 19:08:53 -0300

    refactor and add ctx to middleware auth

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,31 +1,54 @@
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum::{async_trait, RequestPartsExt};
 use lazy_regex::regex_captures;
+use tower_cookies::Cookie;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
 //MW is for middleware
 pub async fn mw_requiure_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
-    println!("->> {:^12} - mw_require_auth", "MIDDLEWARE");
+    println!("->> {:^12} - mw_require_aut - {ctx:?}", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
-    //Parse token
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    //TODO: Token components validation
+    ctx?;
 
     Ok(next.run(req).await)
 }
+
+// Region: -- Ctx extractor
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:^12} - Ctx", "EXTRACTOR");
+
+        //User cookies extractor
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        //Parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        Ok(Ctx::new(user_id))
+    }
+}
+
+// Region: -- Ctx extractor
 
 //Parse a token of format "user-[user-id].[expiration].[signature]"
 //Returns (user_id, expiration, signature)
