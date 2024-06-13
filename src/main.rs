3f32c2e9f0b75861807654291578182f06a21713
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-13 19:58:49 -0300

    implement uuid validation to response_mapper

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod ctx;
 mod error;
 mod model;
 mod web;
+mod log;
 
 //use crate::ctx::Ctx;
 //use crate::log::log_request;
@@ -21,8 +22,8 @@ use std::net::SocketAddr;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 use web::mw_auth;
-//use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -61,9 +62,34 @@ async fn main() -> Result<()> {
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
 
+    let uuid = Uuid::new_v4();
+
+    //Get eventual resposne error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|s| s.client_status_and_error());
+
+    //If client error, build new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+            
+            println!("->> client_error_body: {client_error_body}");
+
+            //Build the new response form the cliente_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    //TODO: Build and log the server log line
+    println!("->> server log line - {uuid} - Error: {service_error:?}");
     println!();
 
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_hello() -> Router {
